bebad1b69cdb70942dbe611988561f52c1261f77
Grusker <grusker@github.com>
2021-06-27 15:24:38 +0300

    physio create and list pages development

--- injury-surveillance-system/src/app/app-routing.module.ts
+++ injury-surveillance-system/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PhysioCreateComponent } from './components/physio/physio-create/physio-create.component';
 import { PhysioListComponent } from './components/physio/physio-list/physio-list.component';
 
 const routes: Routes = [
@@ -9,8 +10,12 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: '',
+    path: 'physio-list',
     component: PhysioListComponent
+  },
+  {
+    path: 'physio-create',
+    component: PhysioCreateComponent
   }
 ];
 
