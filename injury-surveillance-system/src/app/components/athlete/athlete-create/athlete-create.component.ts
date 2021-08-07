ad1b7d321063de7baa3d36a508e19cb55b6c24bb
Grusker <grusker@github.com>
2021-08-07 15:47:31 +0300

    athlete create page html and css update

--- injury-surveillance-system/src/app/components/athlete/athlete-create/athlete-create.component.ts
+++ injury-surveillance-system/src/app/components/athlete/athlete-create/athlete-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
+import { Router } from '@angular/router';
 import { AthleteRequest } from 'src/app/models/AthleteRequest';
 import { BodySide } from 'src/app/models/BodySide';
 import { Gender } from 'src/app/models/Gender';
@@ -9,17 +11,25 @@ import { PhysioService } from 'src/app/services/physio/physio.service';
 @Component({
   selector: 'app-athlete-create',
   templateUrl: './athlete-create.component.html',
-  styleUrls: ['./athlete-create.component.css']
+  styleUrls: ['./athlete-create.component.css', '../../../app.component.css'],
 })
 export class AthleteCreateComponent implements OnInit {
   athleteForm: FormGroup;
   validMessage = '';
 
   physioList = [];
-  genderEnum=[];
-  dominantSideEnum=[];
+  genderEnum = [];
+  dominantSideEnum = [];
 
-  constructor(private athleteService: AthleteService, private physioService: PhysioService) { }
+  selectedPhysio;
+  selectedGender;
+  selectedDominantSide;
+
+  constructor(
+    private athleteService: AthleteService,
+    private physioService: PhysioService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.athleteForm = new FormGroup({
@@ -31,42 +41,56 @@ export class AthleteCreateComponent implements OnInit {
       weight: new FormControl('', Validators.required),
       gender: new FormControl('', Validators.required),
       dominantSide: new FormControl('', Validators.required),
-      physioId: new FormControl('', Validators.required)
+      physioId: new FormControl('', Validators.required),
     });
 
-    this.physioService.getPhysios().subscribe(
-      (data: any) => {
-        this.physioList = data;
-      }
-    )
+    this.physioService.getPhysios().subscribe((data: any) => {
+      this.physioList = data;
+    });
 
     this.genderEnum = Object.keys(Gender);
     this.dominantSideEnum = Object.keys(BodySide);
   }
 
-  createAthlete() {
-    if(this.athleteForm.valid) {
-      let athleteRequest: AthleteRequest = new AthleteRequest();
-      athleteRequest.name = this.athleteForm.get("name").value;
-      athleteRequest.surname = this.athleteForm.get("surname").value;
-      athleteRequest.email = this.athleteForm.get("email").value;
-      athleteRequest.mobile = this.athleteForm.get("mobile").value;
-      athleteRequest.height = this.athleteForm.get("height").value;
-      athleteRequest.weight = this.athleteForm.get("weight").value;
-      athleteRequest.gender = this.athleteForm.get("gender").value;
-      athleteRequest.dominantSide = this.athleteForm.get("dominantSide").value;
-      athleteRequest.physioId = this.athleteForm.get("physioId").value;
-
-      this.athleteService.createAthlete(athleteRequest).subscribe(
-        data => {
-          this.validMessage = "Athlete created successfully.";
-          this.athleteForm.reset();
-          return true;
-        }
-      )
-    } else {
-      this.validMessage = "Please fill the form!";
-    }
+  physioSelection(event: MatSelectChange) {
+    this.athleteForm.patchValue({
+      physioId: event.value,
+    });
   }
 
+  dominantSideSelection(event: MatSelectChange) {
+    this.athleteForm.patchValue({
+      dominantSide: event.value,
+    });
+  }
+
+  genderSelection(event: MatSelectChange) {
+    this.athleteForm.patchValue({
+      gender: event.value,
+    });
+  }
+
+  createAthlete() {
+    if (this.athleteForm.valid) {
+      let athleteRequest: AthleteRequest = new AthleteRequest();
+      athleteRequest.name = this.athleteForm.get('name').value;
+      athleteRequest.surname = this.athleteForm.get('surname').value;
+      athleteRequest.email = this.athleteForm.get('email').value;
+      athleteRequest.mobile = this.athleteForm.get('mobile').value;
+      athleteRequest.height = this.athleteForm.get('height').value;
+      athleteRequest.weight = this.athleteForm.get('weight').value;
+      athleteRequest.gender = this.athleteForm.get('gender').value;
+      athleteRequest.dominantSide = this.athleteForm.get('dominantSide').value;
+      athleteRequest.physioId = this.athleteForm.get('physioId').value;
+
+      this.athleteService.createAthlete(athleteRequest).subscribe((data) => {
+        this.validMessage = 'Athlete created successfully.';
+        this.athleteForm.reset();
+        return true;
+      });
+      this.router.navigate(['athlete-list']);
+    } else {
+      this.validMessage = 'Please fill the form!';
+    }
+  }
 }
--- injury-surveillance-system/src/app/components/physio/physio-create/physio-create.component.ts
+++ injury-surveillance-system/src/app/components/physio/physio-create/physio-create.component.ts
@@ -7,42 +7,39 @@ import { PhysioService } from 'src/app/services/physio/physio.service';
 @Component({
   selector: 'app-physio-create',
   templateUrl: './physio-create.component.html',
-  styleUrls: ['./physio-create.component.css', '../../../app.component.css']
+  styleUrls: ['./physio-create.component.css', '../../../app.component.css'],
 })
 export class PhysioCreateComponent implements OnInit {
   physioForm: FormGroup;
   validMessage = '';
 
-  constructor(private physioService: PhysioService, private router: Router) { }
+  constructor(private physioService: PhysioService, private router: Router) {}
 
   ngOnInit(): void {
     this.physioForm = new FormGroup({
       name: new FormControl('', Validators.required),
       surname: new FormControl('', Validators.required),
       email: new FormControl('', Validators.required),
-      mobile: new FormControl('', Validators.required)
+      mobile: new FormControl('', Validators.required),
     });
   }
 
   createPhysio() {
-    if(this.physioForm.valid) {
+    if (this.physioForm.valid) {
       let physioRequest: PhysioRequest = new PhysioRequest();
-      physioRequest.name = this.physioForm.get("name").value;
-      physioRequest.surname = this.physioForm.get("surname").value;
-      physioRequest.email = this.physioForm.get("email").value;
-      physioRequest.mobile = this.physioForm.get("mobile").value;
+      physioRequest.name = this.physioForm.get('name').value;
+      physioRequest.surname = this.physioForm.get('surname').value;
+      physioRequest.email = this.physioForm.get('email').value;
+      physioRequest.mobile = this.physioForm.get('mobile').value;
 
-      this.physioService.createPhysio(physioRequest).subscribe(
-        data => {
-         this.validMessage = "Physio created succesfully.";
-         this.physioForm.reset();
-         return true; 
-        });
-        this.router.navigate(['physio-list']);
+      this.physioService.createPhysio(physioRequest).subscribe((data) => {
+        this.validMessage = 'Physio created succesfully.';
+        this.physioForm.reset();
+        return true;
+      });
+      this.router.navigate(['physio-list']);
     } else {
-      this.validMessage = "Please fill the form!";
+      this.validMessage = 'Please fill the form!';
     }
-    
   }
-
 }
