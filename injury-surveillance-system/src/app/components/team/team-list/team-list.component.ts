4766924c95bb375fccadfb18987dd2096fd40fd3
Grusker <ce.sekerugur@gmail.com>
2024-01-13 20:15:24 +0100

    page for listing teams are created

--- injury-surveillance-system/src/app/components/team/team-list/team-list.component.ts
+++ injury-surveillance-system/src/app/components/team/team-list/team-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from 'src/app/services/team/team.service';
 
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.css']
+  styleUrls: ['./team-list.component.css', '../../../app.component.css']
 })
 export class TeamListComponent implements OnInit {
+  public teams: any;
+  public displayedColumns: any;
 
-  constructor() { }
+  constructor(private teamService: TeamService) { }
 
   ngOnInit(): void {
+    this.getTeams();
+    this.displayedColumns = ['id', 'name'];
   }
 
+  getTeams() {
+    this.teamService.getTeams().subscribe((data: any) => {
+      this.teams = data;
+      console.log(data);
+    });
+  }
 }
