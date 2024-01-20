1f735cb31a4a3794046a9cb0971c193c1d77c2a6
Grusker <ce.sekerugur@gmail.com>
2024-01-20 20:17:54 +0100

    Team field is added to physio edit page

--- injury-surveillance-system/src/app/components/physio/physio-edit/physio-edit.component.ts
+++ injury-surveillance-system/src/app/components/physio/physio-edit/physio-edit.component.ts
@@ -5,9 +5,12 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PhysioRequest } from 'src/app/models/PhysioRequest';
+import { TeamRequest } from 'src/app/models/TeamRequest';
 import { PhysioService } from 'src/app/services/physio/physio.service';
+import { TeamService } from 'src/app/services/team/team.service';
 
 @Component({
   selector: 'app-physio-edit',
@@ -16,30 +19,36 @@ import { PhysioService } from 'src/app/services/physio/physio.service';
 })
 export class PhysioEditComponent implements OnInit {
   public physioId: number;
-  public physio: PhysioRequest;
   public physioForm: FormGroup;
   validMessage = '';
+  teamList = [];
 
   constructor(
     private route: ActivatedRoute,
     private physioService: PhysioService,
+    private teamService: TeamService,
     private formBuilder: FormBuilder,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
+      this.teamService.getTeams().subscribe((data: any) => {
+        this.teamList = data;
+      })
+
       this.physioId = parseInt(params.get('physioId'));
 
       this.physioService.getPhysio(this.physioId).subscribe((data: any) => {
-        this.physio = data;
 
         this.physioForm = this.formBuilder.group({
-          name: new FormControl(this.physio.name, Validators.required),
-          surname: new FormControl(this.physio.surname, Validators.required),
-          email: new FormControl(this.physio.email, Validators.required),
-          mobile: new FormControl(this.physio.mobile, Validators.required),
+          name: new FormControl(data.name, Validators.required),
+          surname: new FormControl(data.surname, Validators.required),
+          email: new FormControl(data.email, Validators.required),
+          mobile: new FormControl(data.mobile, Validators.required),
+          teamId: new FormControl(data.teamId, Validators.required),
         });
+        this.physioForm.get('teamId').setValue(data.team.id);
       });
     });
   }
@@ -51,6 +60,7 @@ export class PhysioEditComponent implements OnInit {
       updatedPhysio.surname = this.physioForm.get('surname').value;
       updatedPhysio.email = this.physioForm.get('email').value;
       updatedPhysio.mobile = this.physioForm.get('mobile').value;
+      updatedPhysio.teamId = this.physioForm.get('teamId').value;
 
       this.physioService.updatePhysio(this.physioId, updatedPhysio).subscribe((data) => {
         this.validMessage = 'Physio updated succesfully.';
@@ -62,5 +72,11 @@ export class PhysioEditComponent implements OnInit {
     } else {
       this.validMessage = 'Please fill the form!';
     }
+  }
+
+  teamSelection(event: MatSelectChange) {
+    this.physioForm.patchValue({
+      teamId: event.value,
+    });
   }
 }
