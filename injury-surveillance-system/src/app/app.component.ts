67ddb2115d02c202c3bfc6000ca6a2b4c74b0d47
Grusker <ce.sekerugur@gmail.com>
2024-02-03 14:32:48 +0100

    Removed sidenav toggle

--- injury-surveillance-system/src/app/app.component.ts
+++ injury-surveillance-system/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isBigScreen = false;
   title = 'injury-surveillance-system';
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize(): void {
+    this.isBigScreen = window.innerWidth > 500;
+  }
 }
+
+
